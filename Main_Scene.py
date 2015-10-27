bc23c82e77ec85cb7f92d17ca5d60bf0eb8563ea
ParkGS723 <ashurei5@naver.com>
2015-10-27 12:53:26 +0900

    source

--- Main_Scene.py
+++ Main_Scene.py
@@ -9,10 +9,13 @@ button_x, button_y = 0, 0
 name = "MainScene"
 u_Magician = None
 h_Knight = None
+m_meteor = None
 font = None
 gameUI = None
 timer = False
 chk_time = 0.0
+gold = 0
+
 class User_Magician:
 
     TIME_PER_ACTION = 0.5
@@ -25,14 +28,12 @@ class User_Magician:
     def __init__(self):
         self.x, self.y = 95, 560
         self.frame = 0
-        self.life_time = 0.0
         self.total_frames = 0.0
         self.state = self.IDLE
         if User_Magician.u_image == None:
             User_Magician.u_image = load_image('Hero/User_Magician.png')
 
     def update(self, frame_time):
-        self.life_time += frame_time
         self.total_frames += User_Magician.FRAMES_PER_ACTION * User_Magician.ACTION_PER_TIME * frame_time
         if self.state == self.IDLE:
             self.frame = int(self.total_frames) % 6
@@ -41,6 +42,12 @@ class User_Magician:
 
     def draw(self):
         self.u_image.clip_draw(self.frame*118, self.state*100, 120, 120, self.x, self.y)
+
+    def get_bb(self):
+        return self.x - 50, self.y - 50, self.x + 50, self.y + 50
+
+    def draw_bb(self):
+        draw_rectangle(*self.get_bb())
 
 class Hero_Knight:
     PIXEL_PER_METER = (10.0 / 0.3)           # 10 pixel 30 cm
@@ -66,7 +73,6 @@ class Hero_Knight:
             Hero_Knight.h_image = load_image('Hero/Knight_sheet.png')
 
     def update(self, frame_time):
-        self.life_time += frame_time
         distance = Hero_Knight.RUN_SPEED_PPS * frame_time
         self.total_frames += Hero_Knight.FRAMES_PER_ACTION * Hero_Knight.ACTION_PER_TIME * frame_time
         if self.state == self.WALK:
@@ -79,28 +85,102 @@ class Hero_Knight:
             self.dir = 0
             self.x = random.randint(1100, 1103)
             self.state = self.ATTACK
-            #print("Change Time: %f, Total Frames: %d" % (self.life_time, self.total_frames))
         elif self.x < 100:
             self.dir = 1
             self.x = 100
             self.state = self.WALK
-            #print("Change Time: %f, Total Frames: %d" % (self.life_time, self.total_frames))
 
 
     def draw(self):
         self.h_image.clip_draw(self.frame*100, self.state*100, 110, 100, self.x, self.y)
 
+class Hero_Archer:
+    PIXEL_PER_METER = (10.0 / 0.3)           # 10 pixel 30 cm
+    RUN_SPEED_KMPH = 5.0                    # Km / Hour
+    RUN_SPEED_MPM = (RUN_SPEED_KMPH * 1000.0 / 60.0)
+    RUN_SPEED_MPS = (RUN_SPEED_MPM / 60.0)
+    RUN_SPEED_PPS = (RUN_SPEED_MPS * PIXEL_PER_METER)
+
+    TIME_PER_ACTION = 0.5
+    ACTION_PER_TIME = 0.5 / TIME_PER_ACTION
+    FRAMES_PER_ACTION = 6
+    h_image = None
+    WALK, ATTACK, DAMAGE, DIE = 3,2,1,0
+
+    def __init__(self):
+        self.x, self.y = 0, 300
+        self.frame = random.randint(0, 2)
+        self.life_time = 0.0
+        self.total_frames = 0.0
+        self.dir = 0.8
+        self.state = self.WALK
+        if Hero_Archer.h_image == None:
+            Hero_Archer.h_image = load_image('Hero/Archer_sheet.png')
+
+    def update(self, frame_time):
+        distance = Hero_Archer.RUN_SPEED_PPS * frame_time
+        self.total_frames += Hero_Knight.FRAMES_PER_ACTION * Hero_Knight.ACTION_PER_TIME * frame_time
+        if self.state == self.WALK:
+            self.frame = int(self.total_frames) % 6
+        elif self.state == self.ATTACK:
+            self.frame = int(self.total_frames) % 4
+        self.x += (self.dir * distance)
+
+        if self.x > 950:
+            self.dir = 0
+            self.x = random.randint(1100, 1103)
+            self.state = self.ATTACK
+        elif self.x < 100:
+            self.dir = 0.8
+            self.x = 100
+            self.state = self.WALK
+
+
+    def draw(self):
+        self.h_image.clip_draw(self.frame*100, self.state*100, 110, 100, self.x, self.y)
+
+class Magic_Meteor:
+    TIME_PER_ACTION = 0.5
+    ACTION_PER_TIME = 2.0 / TIME_PER_ACTION
+    FRAMES_PER_ACTION = 6
+    m_image = None
+
+    def __init__(self):
+        self.x, self.y = 0, 800
+        self.frame = 0
+        self.total_frames = 0.0
+        self.state = 0
+        if Magic_Meteor.m_image == None:
+            Magic_Meteor.m_image = load_image('Magic/Magic_Meteor.png')
+
+    def update(self, frame_time):
+        self.total_frames += Magic_Meteor.FRAMES_PER_ACTION * Magic_Meteor.ACTION_PER_TIME * frame_time
+        self.frame = int(self.total_frames) % 16
+        if(self.y > 300):
+            self.y -= 0.2
+        elif(self.y < 300):
+            self.y = -100
+
+    def draw(self):
+        self.m_image.clip_draw(self.frame*70, self.state*100, 60, 80, self.x, self.y)
 
 def enter():
-    global h_Knight, font
+    global h_Knight, h_Archer, font
     global u_Magician
-    global stage_background, gameUI, myCastle, enemyCastle, cloud
-    global team1
+    global stage_background, gameUI, myCastle, enemyCastle, cloud, ui_button
+    global my_team
+    global my_magic
+    global m_meteor
 
-    team1 = []
+    my_team = []
+
+    my_magic = []
     u_Magician = User_Magician()
-
     h_Knight = Hero_Knight()
+    h_Archer = Hero_Archer()
+    m_meteor = Magic_Meteor()
+
+    ui_button = load_image('UI/UI_Button.png')
     gameUI = load_image('UI/GameUI.png')
     stage_background = load_image('Map/Stage2_bkg.png')
     myCastle = load_image('Map/MyCastle.png')
@@ -110,15 +190,17 @@ def enter():
     game_framework.reset_time()
 
 def exit():
-    global u_Magician, h_Knight, font, myCastle, enemyCastle, stage_background, cloud, gameUI
+    global u_Magician, h_Knight, font, myCastle, enemyCastle, stage_background, cloud, gameUI, m_meteor
     del(u_Magician)
     del(h_Knight)
+    del(h_Archer)
     del(font)
     del(myCastle)
     del(enemyCastle)
     del(stage_background)
     del(cloud)
     del(gameUI)
+    del(m_meteor)
 
 def pause():
     pass
@@ -126,13 +208,37 @@ def pause():
 def resume():
     pass
 
+def collide(a, b):
+    left_a, bottom_a, right_a, top_a = a.get_bb()
+    left_b, bottom_b, right_b, top_b = b.get_bb()
+
+    if left_a > right_b : return False
+    if right_a < left_b : return False
+    if top_a < bottom_b : return False
+    if bottom_a > top_b : return False
+
+    return True
+
 def button_click():
-    global h_Knight
+    global h_Knight, h_Archer, gold
     if 80 < button_x < 180 and 80 < button_y < 180:
         h_Knight = Hero_Knight()
-        if len(team1) < 10:
-            h_Knight.draw()
-            team1.append(h_Knight)
+        gold = gold - 200
+        if len(my_team) < 50:
+            my_team.append(h_Knight)
+
+    if 230 < button_x < 450 and 80 < button_y < 180:
+        h_Archer = Hero_Archer()
+        gold = gold - 500
+        if len(my_team) < 50:
+            my_team.append(h_Archer)
+
+    if 100 < button_x < 700 and 300 < button_y < 768:
+         m_meteor = Magic_Meteor()
+         if len(my_magic) < 100:
+             m_meteor.x = button_x
+             my_magic.append(m_meteor)
+
 
 
 def handle_events(frame_time):
@@ -154,13 +260,12 @@ def handle_events(frame_time):
                 u_Magician.state = u_Magician.ATTACK
                 timer = False
 
-
-
-
 def update(frame_time):
-    global timer, chk_time
+    global timer, chk_time, gold
     h_Knight.update(frame_time)
     u_Magician.update(frame_time)
+    m_meteor.update(frame_time)
+    gold += (frame_time * 100)
     if timer == False:
         chk_time += frame_time
     if chk_time > 1.0:
@@ -169,16 +274,27 @@ def update(frame_time):
         u_Magician.state = u_Magician.IDLE
 
 def draw(frame_time):
+    global gold
     clear_canvas()
     stage_background.draw(400, 445)
-    gameUI.draw(640, 50)
+    gameUI.draw(640, 70)
     myCastle.draw(100, 355)
     cloud.draw(90, 500)
     enemyCastle.draw(1200, 355)
+    ui_button.draw(1230, 690)
     u_Magician.draw()
-    for h_Knight in team1:
-        h_Knight.update(frame_time)
-        h_Knight.draw()
+    font.draw(551, 56, '%1.f' % gold)
+    for m_meteor in my_magic:
+        m_meteor.update(frame_time)
+        m_meteor.draw()
+
+    for h_Archer in my_team:
+        h_Archer.update(frame_time)
+        h_Archer.draw()
+
+    for h_Archer in my_team:
+        h_Archer.update(frame_time)
+        h_Archer.draw()
     update_canvas()
 
 
